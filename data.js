456c453e34bd78e6e402c9d23a25c2ae92dbbd59
Petr Valenta <petr.valenta.92@gmail.com>
2023-04-04 11:37:25 +0200

    Data from data.js now can be exported

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
-const menuArray = [
+export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        id: 0
     },
     {
         name: "Hamburger",
