6c42b8203c5042b227bc442394004cab2966355d
Petr Valenta <petr.valenta.92@gmail.com>
2023-04-05 09:43:59 +0200

    app now renders whole menu

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 import { menuArray } from './data.js';
+
+function getFeedHtml() {
+     let feedHtml = '';
+
+     menuArray.forEach(function(meal){
+        feedHtml += `
+        <div class="container meal">
+            <p class="meal-img">${meal.emoji}</p>
+                <div class="meal-details">
+                    <h3 class="meal-title">${meal.name}</h3>
+                    <p class="meal-ingrediences">${meal.ingredients}</p>
+                    <p class="meal-price">${meal.price}$</p>
+                </div>
+                <button class="add-btn">+</button>
+        </div>`
+     })
+
+     return feedHtml;
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml();
+}
+
+render(); 
+
+
