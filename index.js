53d517a23c0bb5894379f0577792ab9f0c382374
Petr Valenta <petr.valenta.92@gmail.com>
2023-04-11 09:44:04 +0200

    Ordered meal render fix

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 import { menuArray } from './data.js';
 
-const checkoutOrder = document.getElementById('checkout-order');
-let orderMealHtml = '';
-let checkoutHtml = ''; 
+let orderMealArray = [];
 let totalPrice = 0;
 
 document.getElementById('menu').addEventListener('click', function(e){
 
     menuArray.forEach(function(meal){
 
-        if (e.target.id === `add-btn-${meal.id}`) {
+        if (e.target.id === `increment-btn-${meal.id}`) {
             showCheckout(); 
+            document.getElementById(`decrement-btn-${meal.id}`).disabled = false;
 
-            if (checkoutOrder.contains(document.getElementById(`meal-${meal.id}`))) {
+            if (!orderMealArray.includes(meal)){
+                orderMealArray.push(meal);
                 addOneToMealQuantity(meal);
-                renderNewQuantity(meal);
             } else {
                 addOneToMealQuantity(meal);
-                renderOrderMeal(meal);
             }
 
-            console.log(orderMealHtml);
-    
+            renderOrderMeal();
             totalPrice += meal.price;
-
             renderSummary();
-        }       
+        }
+        
+        if (e.target.id === `decrement-btn-${meal.id}`) {
+
+        }
     });      
 });
 
@@ -33,6 +33,7 @@ function getMenuHtml() {
      let menuHtml = '';
 
      menuArray.forEach(function(meal){
+
         menuHtml += `
         <div class="container meal">
             <p class="meal-img">${meal.emoji}</p>
@@ -42,8 +43,8 @@ function getMenuHtml() {
                     <p class="meal-price">${meal.price}$</p>
                 </div>
                 <div class="meal-buttons">
-                    <button id=add-btn-${meal.id} class="meal-btn">+</button>
-                    <button id=remove-btn-${meal.id} class="meal-btn" disabled>-</button>
+                    <button id=increment-btn-${meal.id} class="meal-btn">+</button>
+                    <button id=decrement-btn-${meal.id} class="meal-btn" disabled>-</button>
                 </div>
         </div>`;
      })
@@ -64,43 +65,32 @@ function hideCheckout(){
     document.getElementById('checkout').style.display = 'none';
 };
 
-function getOrderMealHtml(meal) { 
 
-    orderMealHtml += `
-        <div id="meal-${meal.id}" class="checkout-meal">
-            <p class="meal-title">${meal.name}</p>
-            <button id="remove-meal-btn" class="remove-meal-btn">remove</button>
-            <div>
-                <p class="meal-count">${meal.quantity}x</p>
-                <p class="meal-price">$${meal.price*meal.quantity}</p>
-            </div>
-        </div>`;
+function getOrderMealHtml() { 
+    let orderMealHtml = '';
+    orderMealArray.forEach(function(meal){
 
-        return orderMealHtml;
+            orderMealHtml += `
+                <div id="meal-${meal.id}" class="checkout-meal">
+                    <p class="meal-title">${meal.name}</p>
+                    <button id="remove-meal-btn" class="remove-meal-btn">remove</button>
+                    <div>
+                        <p class="meal-count">${meal.quantity}x</p>
+                        <p class="meal-price">$${meal.price*meal.quantity}</p>
+                    </div>
+                </div>`;
+    })
+
+    return orderMealHtml;
 };
 
-function renderOrderMeal(meal) {
-    document.getElementById('checkout-order').innerHTML = getOrderMealHtml(meal);
-}
-
-function getNewQuantityHtml(meal) {
-    let newQuantityHtml = `
-        <p class="meal-title">${meal.name}</p>
-        <button id="remove-meal-btn" class="remove-meal-btn">remove</button>
-        <div>
-            <p class="meal-count">${meal.quantity}x</p>
-            <p class="meal-price">$${meal.price*meal.quantity}</p>
-        </div>
-    `
-
-    return newQuantityHtml;
-}
-
-function renderNewQuantity(meal) {
-    document.getElementById(`meal-${meal.id}`).innerHTML = getNewQuantityHtml(meal);
+function renderOrderMeal() {
+    document.getElementById('checkout-order').innerHTML = getOrderMealHtml();
 }
 
 function getSummaryHtml() {
+    let checkoutHtml = '';
+
     checkoutHtml = `
         <p class="checkout-price-text">Total price:</p>
         <p class="checkout-price">${totalPrice}$</p>
