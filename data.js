ac6a87e478ac93ac54f2c2256bf9528a6e4dfb4c
Petr Valenta <petr.valenta.92@gmail.com>
2023-04-20 08:51:26 +0200

    Add new meals

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 export const menuArray = [
-    {
+        {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         price: 14,
@@ -7,7 +7,7 @@ export const menuArray = [
         id: 0,
         quantity: 0
     },
-    {
+        {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
@@ -16,19 +16,53 @@ export const menuArray = [
         quantity: 0
     },
         {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
+        name: "Hot Wings",
+        ingredients: ["chicken wings", "hot BBQ sauce"],
         price: 12,
-        emoji: "🍺",
+        emoji: "🍗",
         id: 2,
         quantity: 0
     },
         {
-        name: "Lemonade",
-        ingredients: ["water, sugar, lemon"],
+        name: "HotDog",
+        ingredients: ["bun", "sausage", "mustard", "onion", "pickle"],
         price: 8,
-        emoji: "🥤",
+        emoji: "🌭",
         id: 3,
         quantity: 0
-    }
+    },
+        {
+        name: "Fries",
+        ingredients: ["potatoes", "ketchup"],
+        price: 8,
+        emoji: "🍟",
+        id: 4,
+        quantity: 0
+    },
+        
+        {
+        name: "Beer",
+        ingredients: ["grain", "hops", "yeast", "water"],
+        price: 10,
+        emoji: "🍺",
+        id: 5,
+        quantity: 0
+    },
+        {
+        name: "Margarita",
+        ingredients: ["tequila", "lime juice", "cointreau", "salt"],
+        price: 12,
+        emoji: "🍹",
+        id: 6,
+        quantity: 0
+    },
+        {
+        name: "Lemonade",
+        ingredients: ["water", "sugar", "lemon juice"],
+        price: 8,
+        emoji: "🥤",
+        id: 7,
+        quantity: 0
+    } 
+        
 ]
